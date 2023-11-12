98860a39f48e8a73426d3b964683c92a6e81dc09
fm <felix.mueller@gwendesign.ch>
2023-11-12 20:48:35 +0100

    Fix PaHUB standalone

--- m5stack/libs/unit/pahub.py
+++ m5stack/libs/unit/pahub.py
@@ -25,7 +25,7 @@ class PAHUBUnit:
     _addr = PAHUB_DEFAULT_ADDR
 
     def __init__(
-        self, i2c: I2C, channel: Literal[0, 1, 2, 3, 4, 5] = 0, address=PAHUB_DEFAULT_ADDR
+        self, i2c: I2C, channel: Literal[-1, 0, 1, 2, 3, 4, 5] = -1, address=PAHUB_DEFAULT_ADDR
     ) -> None:
         self._i2c = i2c
         self._chn = channel
@@ -45,67 +45,91 @@ class PAHUBUnit:
         self.release_channel(self._chn)
 
     def scan(self) -> list[int]:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.scan()
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def start(self) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.start()
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
 
     def stop(self) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.stop()
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
 
     def readinto(self, buf: AnyWritableBuf, nack: bool = True) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.readinto(buf, nack)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
 
     def write(self, buf: AnyReadableBuf) -> int:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.write(buf)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def readfrom(self, addr: int, nbytes: int, stop: bool = True) -> bytes:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.readfrom(addr, nbytes, stop)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def readfrom_into(self, addr: int, buf: AnyWritableBuf, stop: bool = True) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.readfrom_into(addr, buf, stop)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
 
     def writeto(self, addr: int, buf: AnyReadableBuf, stop: bool = True) -> int:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.writeto(addr, buf, stop)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def writevto(self, addr: int, vector: Sequence[AnyReadableBuf], stop: bool = True) -> int:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.writevto(addr, vector, stop)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def readfrom_mem(self, addr: int, memaddr: int, nbytes: int) -> bytes:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         result = self._i2c.readfrom_mem(addr, memaddr, nbytes)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
         return result
 
     def readfrom_mem_into(self, addr: int, memaddr: int, buf: AnyWritableBuf) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.readfrom_mem_into(addr, memaddr, buf)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
 
     def writeto_mem(self, addr: int, memaddr: int, buf: AnyReadableBuf) -> None:
-        self.select_channel(self._chn)
+        if self._chn != -1:
+            self.select_channel(self._chn)
         self._i2c.writeto_mem(addr, memaddr, buf)
-        self.release_channel(self._chn)
+        if self._chn != -1:
+            self.release_channel(self._chn)
