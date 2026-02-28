e3fe59446e2866bffd5413af23eec3cddb44d03d
tinyu <tinyu.zhao@gmail.com>
2026-02-28 10:56:18 +0800

    boards: Add Hat module for Unit PoEP4.
    Signed-off-by: tinyu <tinyu.zhao@gmail.com>

--- m5stack/boards/M5STACK_Unit_PoEP4/manifest.py
+++ m5stack/boards/M5STACK_Unit_PoEP4/manifest.py
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: MIT
 
 include("$(MPY_DIR)/../m5stack/modules/startup/manifest_unit_poep4.py")
+include("$(MPY_DIR)/../m5stack/libs/hat/manifest.py")
 include("$(MPY_DIR)/../m5stack/libs/unit/manifest.py")
 include("$(MPY_DIR)/../m5stack/libs/chain/manifest.py")
-include("$(MPY_DIR)/../m5stack/libs/usb/manifest.py")
