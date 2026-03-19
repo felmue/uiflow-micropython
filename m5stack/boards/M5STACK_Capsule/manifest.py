f31ec8a750204149689ff555f1df82c2fdaa49ba
lbuque <1102390310@qq.com>
2026-03-19 10:14:22 +0800

    boards: Fix USB module import failure.
    Signed-off-by: lbuque <1102390310@qq.com>

--- m5stack/boards/M5STACK_Capsule/manifest.py
+++ m5stack/boards/M5STACK_Capsule/manifest.py
@@ -5,3 +5,4 @@
 include("$(MPY_DIR)/../m5stack/modules/startup/manifest_capsule.py")
 include("$(MPY_DIR)/../m5stack/libs/unit/manifest.py")
 include("$(MPY_DIR)/../m5stack/libs/chain/manifest.py")
+include("$(MPY_DIR)/../m5stack/libs/usb/manifest.py")
--- m5stack/boards/M5STACK_Cardputer/manifest.py
+++ m5stack/boards/M5STACK_Cardputer/manifest.py
@@ -5,3 +5,4 @@
 include("$(MPY_DIR)/../m5stack/modules/startup/manifest_cardputer.py")
 include("$(MPY_DIR)/../m5stack/libs/unit/manifest.py")
 include("$(MPY_DIR)/../m5stack/libs/chain/manifest.py")
+include("$(MPY_DIR)/../m5stack/libs/usb/manifest.py")
